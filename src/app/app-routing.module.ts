2c5d2957b24af570d352398b3b0075517c5b8516
GrandmaCoding <jmetzger5@gmail.com>
2022-04-19 11:24:16 -0500

    Login, myaccount, and register components routed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { MyaccountComponent } from './myaccount/myaccount.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login',component: LoginComponent},
+  {path: 'myaccount',component: MyaccountComponent},
+  {path: 'register',component: RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
