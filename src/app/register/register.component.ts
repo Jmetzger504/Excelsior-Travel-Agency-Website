f0d77112c367fa6cc12df8fd2a61998dbad4061e
GrandmaCoding <jmetzger5@gmail.com>
2022-04-21 11:48:12 -0500

    Registration validation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -9,24 +9,49 @@ import {FormControl, Validators,ReactiveFormsModule} from '@angular/forms';
 export class RegisterComponent implements OnInit {
 
   hide:boolean;
-  email = new FormControl('', [Validators.required, Validators.email]);
+ 
   constructor() {
     this.hide = true;
    }
 
-   password = new FormControl('',Validators.compose([
+  firstName = new FormControl('',Validators.compose([Validators.required,Validators.minLength(2)]));
+  lastName = new FormControl('',Validators.compose([Validators.required,Validators.minLength(2)]));
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('',Validators.compose([
      Validators.required,
-     Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$")]));
+     Validators.minLength(8)]));
    
+  zipCode = new FormControl('',Validators.compose([Validators.required,Validators.min(501),Validators.max(99950),Validators.minLength(5)]))
 
-  getErrorMessage() {
+  getZipCodeErrorMessage() {
+    if(this.zipCode.hasError('required'))
+      return 'You must enter a zip code.'
+    return this.zipCode.hasError('min' || 'max' || 'minLength(5)') ? 'Please enter a valid zip code.' : '';
+  }
+
+  getEmailErrorMessage() {
     if (this.email.hasError('required')) {
-      return 'You must enter a value';
+      return 'You must enter an email';
     }
 
     return this.email.hasError('email') ? 'Not a valid email' : '';
   }
+
+  getPasswordErrorMessage() {
+    if(this.password.hasError('required')) {
+      return 'You must enter a password';
+    }
+    return 'Must be at least 8 characters.';
+  }
+
+  getFirstNameErrorMessage() {
+    return 'Please enter a valid name (2 characters or more).';
+  }
   
+  getLastNameErrorMessage() {
+    return 'Please enter a valid name (2 characters or more).';
+  }
+
   ngOnInit(): void {
   }
 
